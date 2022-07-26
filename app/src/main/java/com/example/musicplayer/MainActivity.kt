017df8be1764883ae9f59a525b3c49f3c2124edd
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-07-26 13:37:28 +0430

    completing ActionBar code and fix Slider bug

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.musicplayer
 
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.example.musicplayer.databinding.ActivityMainBinding
 import com.google.android.material.slider.Slider
 import java.util.*
@@ -11,7 +14,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var mediaPlayer: MediaPlayer
     private var isPlaying = false
-    private var isUserChanging = false
+    private var muted=false
+    lateinit var timer: Timer
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -36,20 +40,20 @@ class MainActivity : AppCompatActivity() {
         binding.SliderMain.valueTo = mediaPlayer.duration.toFloat()
 
         //Slider=>
-        val timer = Timer()
+        timer = Timer()
         timer.schedule(object : TimerTask() {
             //running on thread other
             override fun run() {
                 //running on thread Ui(Main)
                 runOnUiThread {
-                    if (!isUserChanging) {
-                        binding.SliderMain.value = mediaPlayer.currentPosition.toFloat()
-                    }
+
+                    binding.SliderMain.value = mediaPlayer.currentPosition.toFloat()
+
                 }
             }
 
         }, 1000, 1000)
-        //Change media player while changing slider=>
+        //Change media player while slider changing=>
         binding.SliderMain.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
             override fun onStartTrackingTouch(slider: Slider) {
 
@@ -62,7 +66,6 @@ class MainActivity : AppCompatActivity() {
         //Time start=>
         binding.SliderMain.addOnChangeListener { slider, value, fromUser ->
             binding.txtStartTime.text = convertMillisToString(value.toLong())
-            isUserChanging = fromUser
         }
 
         //Time End =>
@@ -91,11 +94,34 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        binding.btnGoBefore.setOnClickListener { }
+        binding.btnGoBefore.setOnClickListener {
+            val now = mediaPlayer.currentPosition
+            val before = now - 15000
+            mediaPlayer.seekTo(before)
+        }
 
-        binding.btnGoAfter.setOnClickListener { }
+        binding.btnGoAfter.setOnClickListener {
+            val now = mediaPlayer.currentPosition
+            val after = now + 15000
+            mediaPlayer.seekTo(after)
+        }
 
-        binding.btnSound.setOnClickListener { }
+        binding.btnSound.setOnClickListener {
+            val audioManger=getSystemService(AUDIO_SERVICE) as AudioManager
+            if (muted){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    audioManger.adjustVolume(AudioManager.ADJUST_UNMUTE,AudioManager.FLAG_SHOW_UI)
+                    binding.btnSound.setImageResource(R.drawable.ic_volume_on)
+                    muted=false
+                }
+            }else{
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    audioManger.adjustVolume(AudioManager.ADJUST_MUTE,AudioManager.FLAG_SHOW_UI)
+                    binding.btnSound.setImageResource(R.drawable.ic_volume_off)
+                    muted=true
+                }
+            }
+        }
 
     }
 }
